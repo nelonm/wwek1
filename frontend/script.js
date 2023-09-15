5c83da97d8615e615f49cceefa47e85b13c8a64d
nelonm <acsigy@gmail.com>
2023-09-15 20:45:10 +0200

    basic layout, practice

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("I am Imola");
+//setInterval(()=> console.log("Hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x+y;
+let z = "csésze"
+console.log(z)
+z = "bögre"
+console.log(z)
